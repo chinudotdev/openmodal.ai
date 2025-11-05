b675949cfc3cd00077afa399f0d65ed350a84a86
chinu <chinudotdev@gmail.com>
2025-11-06 00:10:55 +0530

    add: forgot password page

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -167,12 +167,12 @@ export function LoginForm({
                     <Field data-invalid={isInvalid}>
                       <div className="flex items-center">
                         <FieldLabel htmlFor={field.name}>Password</FieldLabel>
-                        {/* <a
-                          href="#"
+                        <Link
+                          href="/forgot-password"
                           className="ml-auto text-sm underline-offset-4 hover:underline"
                         >
                           Forgot your password?
-                        </a> */}
+                        </Link>
                       </div>
                       <Input
                         id={field.name}
