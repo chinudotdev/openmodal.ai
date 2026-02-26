ce99a3433c0e1f26c5b58c00cdcc80826c6915bf
chinu <chinudotdev@gmail.com>
2026-02-26 20:37:39 +0530

    fix: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,3 @@
-import { env } from 'cloudflare:workers'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
@@ -6,6 +5,6 @@ export default defineConfig({
   schema: './src/db/schema/index.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 })
