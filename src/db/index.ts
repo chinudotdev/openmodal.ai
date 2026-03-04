c5c2efb747f2a739516b4fc993c3650ed00a7ffa
chinu <chinudotdev@gmail.com>
2026-03-04 16:53:45 +0530

    debug: test connection string with DATABASE_URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,12 @@
 import { env } from 'cloudflare:workers'
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
+
+// import { Pool } from 'pg'
 
 export function dbClient() {
-  console.log({ connection: env.HYPERDRIVE.connectionString })
-  const pool = new Pool({
-    connectionString: env.DATABASE_URL,
-  })
-  return drizzle({ client: pool })
+  // console.log({ connection: env.HYPERDRIVE.connectionString })
+  // const pool = new Pool({
+  //   connectionString: env.DATABASE_URL,
+  // })
+  return drizzle(env.DATABASE_URL)
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,15 @@
 import {
+  createRootRoute,
   HeadContent,
   Link,
   Scripts,
-  createRootRoute,
 } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import appCss from '../styles.css?url'
 import { Button } from '@/components/ui/button'
 import { Spinner } from '@/components/ui/spinner'
 
+import appCss from '../styles.css?url'
 
 export const Route = createRootRoute({
   head: () => ({
