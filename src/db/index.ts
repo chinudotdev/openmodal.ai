f459d93f06ec104a8fd0557091cb9d70d144dfd9
chinu <chinudotdev@gmail.com>
2026-03-04 18:24:16 +0530

    fix: move dbClient to it's own file #2

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { env } from 'cloudflare:workers'
 import { drizzle } from 'drizzle-orm/node-postgres'
 
-export const db = drizzle(env.HYPERDRIVE.connectionString)
+// export const db = drizzle(env.HYPERDRIVE.connectionString)
 
 export const dbClient = () => {
   return drizzle(env.HYPERDRIVE.connectionString)
