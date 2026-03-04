5513ed69caab0ea1827a97cbaab85c8d68be1713
chinu <chinudotdev@gmail.com>
2026-03-04 09:43:54 +0530

    fix: db.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,4 @@
 import { env } from 'cloudflare:workers'
 import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
 
-const client = postgres(env.HYPERDRIVE.connectionString, { max: 1 })
-export const db = drizzle({ client })
+export const db = drizzle(env.HYPERDRIVE.connectionString)
