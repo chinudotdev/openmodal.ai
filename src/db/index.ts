e180f480301f87e914d8a1155635e80d574a7211
chinu <chinudotdev@gmail.com>
2026-03-04 16:29:24 +0530

    debug: test connection string with HYPERDRIVE

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 
 export function dbClient() {
+  console.log({ connection: env.HYPERDRIVE.connectionString })
   const pool = new Pool({
     connectionString: env.HYPERDRIVE.connectionString,
   })
