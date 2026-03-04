764201e726d45cf7ab33f64ea5aec2fecdf42146
chinu <chinudotdev@gmail.com>
2026-03-04 18:11:44 +0530

    test: HYPERDRIVE connection with drizzle

--- src/routes/api/hello.ts
+++ src/routes/api/hello.ts
@@ -1,13 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 import { env } from 'cloudflare:workers'
+import { sql } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Client } from 'pg'
 
 export const Route = createFileRoute('/api/hello')({
   server: {
     handlers: {
-      GET: () => {
-        const connection_url = env.HYPERDRIVE.connectionString
-        return new Response(connection_url)
+      GET: async () => {
+        const client = new Client({
+          connectionString: env.HYPERDRIVE.connectionString,
+        })
+
+        // Connect to the database
+        await client.connect()
+
+        // Create the Drizzle client with the node-postgres connection
+        const db = drizzle(client)
+
+        await db.execute(sql`SELECT 1`)
+        return new Response('OK')
       },
     },
   },
