3504d7b7eff6a23169aff4cc6db17212cd10ad1f
chinu <chinudotdev@gmail.com>
2026-03-04 17:26:15 +0530

    debug: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
 
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
@@ -10,7 +10,7 @@ import viteTsConfigPaths from 'vite-tsconfig-paths'
 const config = defineConfig({
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    cloudflare({ viteEnvironment: { name: 'ssr' }, remoteBindings: true }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
