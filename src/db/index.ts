e29c226a17459ed203aedeab9ef09acbbbd77393
chinu <chinudotdev@gmail.com>
2026-03-04 12:33:45 +0530

    debug: test pg driver with pool

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,10 @@
 import { env } from 'cloudflare:workers'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
 export function dbClient() {
-  return drizzle(env.HYPERDRIVE.connectionString)
+  const pool = new Pool({
+    connectionString: env.HYPERDRIVE.connectionString,
+  })
+  return drizzle({ client: pool })
 }
